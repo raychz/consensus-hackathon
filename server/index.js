80737254e22952e7085e02d86350f9a1c268059f
Chavez, Raymond, III <rchavez@draper.com>
2018-05-13 02:24:47 -0400

    Debugging with hello world response in azure

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  // res.sendFile(path.join(__dirname, 'index.html'));
+  res.send('<p>Hello world</p>');
 });
 
 
